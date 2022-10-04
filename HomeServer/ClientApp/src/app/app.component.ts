110bba594aa14feb18c332e882a86fd26f69983a
Lars <lars.kraemer@live.de>
2022-10-04 14:54:28 +0200

    HOME-3: Angular with Nebular UI

--- HomeServer/ClientApp/src/app/app.component.ts
+++ HomeServer/ClientApp/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MENU_ITEMS } from './pages/pages-menu';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  menu = MENU_ITEMS;
 }
